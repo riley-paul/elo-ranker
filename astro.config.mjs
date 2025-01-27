190dafb095b783dc937e4ccdeb529ea9ad878ce4
Riley Paul <rileypaul96@gmail.com>
2025-01-26 21:50:11 -0700

    add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [react(), tailwind({ applyBaseStyles: false }), db()],
 });
